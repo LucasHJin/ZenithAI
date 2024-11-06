ad0623f066ad7fbbbf56d78c03c39f6102962e9a
Lucas Jin <kblazer20@gmail.com>
2024-11-06 12:34:49 -0500

    Revert "Minimal change - added sections for media"
    This reverts commit fd51c5ce7750d7dbd252fa102501d531be93c7f5.

--- frontend/src/pages/Media.jsx
+++ frontend/src/pages/Media.jsx
@@ -4,11 +4,7 @@ import '../styles/Media.css'
 
 function Media() {
   return (
-    <>
-      <h1>Videos</h1>
-      <h1>Accounts</h1>
-      <h1>Tags/Trends</h1>
-    </>
+    <div>Social Media</div>
   );
 }
 
